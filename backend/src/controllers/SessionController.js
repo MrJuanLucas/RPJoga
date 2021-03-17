064263fd80fc5412930742224d1822721ca9be16
Juan Lucas <juanlabezerra@hotmail.com>
2021-03-16 22:03:33 -0300

    Backend alterado para corrigir falhas na integração com frontend

--- backend/src/controllers/SessionController.js
+++ backend/src/controllers/SessionController.js
@@ -3,16 +3,17 @@ const connection = require('../database/connection');
 module.exports = {
     async create(request, response){
        const {email, password} = request.body;
-
-       const check = await connection('user')
+    
+       const user = await connection('user')
         .where('email',email)
         .where('password', password)
-        .select('name').first();
+        .select('user_id').first()
+        ;
        
-       if(!check){
+       if(!user){
            return response.status(400).json('Senha ou e-mail incorretos')
        } 
 
-       return response.json({check})
+       return response.json({user})
     }
 }
--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -5,6 +5,12 @@ module.exports = {
         
         const {name, email, password} = request.body;
 
+        if(name == '' || email=='' || password==''){
+            return response.status(400).json(
+                {error:'preencha todos os campos para o login'}
+            )
+        }
+
         const email_test = await connection('user')
             .where('email', email)
             .select('email').first();
